bce91dc8980faa2c20eadc13c7a92c5fb9c5ca95
Sangil Park <tkddlf59@gmail.com>
2017-01-17 19:08:43 +0900

    second commit

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,11 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(
                         default=timezone.now)
-    publisged_date = models.DateTimeField(
+    published_date = models.DateTimeField(
                         blank=True, null=True)
 
     def publish(self):
-        self.publisged_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
